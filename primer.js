cb35eee8de3b609ee682cf085771bafb0b7847e1
ЫSergey Pashnin <sspinit88@gmail.com>
2018-11-17 22:47:02 +0300

    lesson 1 fin

--- primer.js
+++ primer.js
@@ -1,1 +1,13 @@
-console.log('hello!');
+// lesson #1
+// string
+var str = 'str';
+// number
+var num = 1;
+// boolean
+var bool = true;
+// other
+var unknown = 1;
+var other = 1;
+console.log('lesson-1: ' + ' ' + typeof str + ' ' + typeof num + ' ' + typeof bool + ' ' + typeof unknown + ' ' + typeof other);
+// lesson #2
+var strArr = ['1', '2', '3'], numArr = [1, 2, 3], boolArr = [true, false];
--- primer.ts
+++ primer.ts
@@ -1,1 +1,21 @@
-console.log('hello!');
+// lesson #1
+// string
+let str: string = 'str';
+// number
+let num: number = 1;
+// boolean
+let bool: boolean = true;
+
+// other
+let unknown: any = 1;
+let other: string | number = 1;
+
+console.log('lesson-1: ' + ' ' + typeof str + ' ' + typeof num + ' ' + typeof bool + ' ' + typeof unknown + ' ' + typeof other);
+
+// lesson #2
+let strArr: string[] = ['1', '2', '3'],
+    numArr: number[] = [1, 2, 3],
+    boolArr: boolean[] = [true, false];
+
+
+
