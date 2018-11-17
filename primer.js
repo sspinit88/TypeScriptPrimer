241dd5c87658a7de929394d455e9eac8cfb20573
ЫSergey Pashnin <sspinit88@gmail.com>
2018-11-17 23:22:27 +0300

    lesson 23 fin

--- primer.js
+++ primer.js
@@ -8,9 +8,45 @@ var bool = true;
 // other
 var unknown = 1;
 var other = 1;
-console.log('lesson-1: ' + ' ' + typeof str + ' ' + typeof num + ' ' + typeof bool + ' ' + typeof unknown + ' ' + typeof other);
+// console.log('lesson-1: ' + ' ' + typeof str + ' ' + typeof num + ' ' + typeof bool + ' ' + typeof unknown + ' ' + typeof other);
 // lesson #2 - типизация массива
 var strArr = ['1', '2', '3'], strArr0 = ['1', '2', '3'], numArr = [1, 2, 3], numArr0 = [1, 2, 3], boolArr = [true, false], boolArr0 = [true, false];
 // tuples
 var array = [1, '2', '3'];
-// lesson #3
+// lesson #3 - function
+var myName = 'Kir';
+var myAge = 23;
+// function getMyName(): string {} - функция возвращает строку.
+function getMyName() {
+    return myName;
+}
+console.log(getMyName());
+function getMyAge() {
+    return myAge;
+    // return myName;
+}
+console.log(getMyAge());
+// ts позволяет указывать тип, входящих в функцию, данных
+// параметры передавать в строгом порядке
+// number | string - тип: или число, или строка
+function getMyAge2(age, prefix) {
+    if (prefix === void 0) { prefix = 'Возраст'; }
+    return prefix + " " + age;
+}
+console.log(getMyAge2(40));
+// функция consoleLog() ничего не возвращает
+// для таких ситуаций предусмотрен специальный тип
+// void - тип, говорящий, что мы ничего не ожидаем в ответ от функции
+function consoleLog(any) {
+    console.log(any);
+}
+consoleLog('start console.log!');
+// mySum станет функцией;
+// let mySum;
+var mySum;
+function sum(num1, num2) {
+    return num1 + num2;
+}
+mySum = sum;
+console.log(sum(2, 3));
+console.log(mySum(2, 3));
--- primer.ts
+++ primer.ts
@@ -10,7 +10,8 @@ let bool: boolean = true;
 let unknown: any = 1;
 let other: string | number = 1;
 
-console.log('lesson-1: ' + ' ' + typeof str + ' ' + typeof num + ' ' + typeof bool + ' ' + typeof unknown + ' ' + typeof other);
+// console.log('lesson-1: ' + ' ' + typeof str + ' ' + typeof num + ' ' + typeof bool + ' ' + typeof unknown + ' ' + typeof other);
+
 
 // lesson #2 - типизация массива
 let strArr: string[] = ['1', '2', '3'],
@@ -25,7 +26,53 @@ let strArr: string[] = ['1', '2', '3'],
 // tuples
 let array: [number, string, string] = [1, '2', '3'];
 
-// lesson #3
 
+// lesson #3 - function
+const myName: string = 'Kir';
+const myAge: number = 23;
 
+// function getMyName(): string {} - функция возвращает строку.
+function getMyName(): string {
+    return myName;
+}
 
+console.log(getMyName());
+
+function getMyAge(): number {
+    return myAge;
+    // return myName;
+}
+
+console.log(getMyAge());
+
+// ts позволяет указывать тип, входящих в функцию, данных
+// параметры передавать в строгом порядке
+// number | string - тип: или число, или строка
+function getMyAge2(age: number, prefix: string = 'Возраст'): number | string {
+    return prefix + " " + age;
+
+}
+
+console.log(getMyAge2(40));
+
+// функция consoleLog() ничего не возвращает
+// для таких ситуаций предусмотрен специальный тип
+// void - тип, говорящий, что мы ничего не ожидаем в ответ от функции
+function consoleLog(any: any): void {
+    console.log(any);
+}
+
+consoleLog('start console.log!');
+
+// mySum станет функцией;
+// let mySum;
+let mySum: (numb1: number, numb2: number) => number;
+
+function sum(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+mySum = sum;
+
+console.log(sum(2, 3));
+console.log(mySum(2, 3));
