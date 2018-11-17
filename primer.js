9fb0ef8a6bd248bd97303d09fc2bb89ec69d65b9
ЫSergey Pashnin <sspinit88@gmail.com>
2018-11-17 22:55:21 +0300

    lesson 2 аÑfin

--- primer.js
+++ primer.js
@@ -1,4 +1,4 @@
-// lesson #1
+// lesson #1 - объявление переменной и типизация
 // string
 var str = 'str';
 // number
@@ -9,5 +9,8 @@ var bool = true;
 var unknown = 1;
 var other = 1;
 console.log('lesson-1: ' + ' ' + typeof str + ' ' + typeof num + ' ' + typeof bool + ' ' + typeof unknown + ' ' + typeof other);
-// lesson #2
-var strArr = ['1', '2', '3'], numArr = [1, 2, 3], boolArr = [true, false];
+// lesson #2 - типизация массива
+var strArr = ['1', '2', '3'], strArr0 = ['1', '2', '3'], numArr = [1, 2, 3], numArr0 = [1, 2, 3], boolArr = [true, false], boolArr0 = [true, false];
+// tuples
+var array = [1, '2', '3'];
+// lesson #3
--- primer.ts
+++ primer.ts
@@ -1,4 +1,4 @@
-// lesson #1
+// lesson #1 - объявление переменной и типизация
 // string
 let str: string = 'str';
 // number
@@ -12,10 +12,20 @@ let other: string | number = 1;
 
 console.log('lesson-1: ' + ' ' + typeof str + ' ' + typeof num + ' ' + typeof bool + ' ' + typeof unknown + ' ' + typeof other);
 
-// lesson #2
+// lesson #2 - типизация массива
 let strArr: string[] = ['1', '2', '3'],
+    strArr0: Array<string> = ['1', '2', '3'],
+
     numArr: number[] = [1, 2, 3],
-    boolArr: boolean[] = [true, false];
+    numArr0: Array<number> = [1, 2, 3],
+
+    boolArr: boolean[] = [true, false],
+    boolArr0: Array<boolean> = [true, false];
+
+// tuples
+let array: [number, string, string] = [1, '2', '3'];
+
+// lesson #3
 
 
 
